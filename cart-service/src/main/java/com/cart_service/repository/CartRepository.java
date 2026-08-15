7ed2d61d9be75b384c7594f9e5a8a60ef61dc04d
aliazamx21 <aliazamgo754@gmail.com>
2026-08-15 14:12:09 +0530

    fix: consolidate CartRepository methods and clean up duplicates

--- cart-service/src/main/java/com/cart_service/repository/CartRepository.java
+++ cart-service/src/main/java/com/cart_service/repository/CartRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUuid(String uuid);
+    Optional<Cart> findByUsername(String username);
 }
